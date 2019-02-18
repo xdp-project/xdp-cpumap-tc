1180f850c4d3245ba1b93b9c1821117f14588e51
Jesper Dangaard Brouer <netoptimizer@brouer.com>
2019-02-18 23:02:28 +0100

    tc_queue_mapping_kern: document how to debug via perf probe
    Signed-off-by: Jesper Dangaard Brouer <netoptimizer@brouer.com>

--- src/tc_queue_mapping_kern.c
+++ src/tc_queue_mapping_kern.c
@@ -11,7 +11,7 @@
 
 */
 
-#define DEBUG 1
+//#define DEBUG 1
 #ifdef  DEBUG
 /* Only use this for debug output. Notice output from bpf_trace_printk()
  * end-up in /sys/kernel/debug/tracing/trace_pipe
