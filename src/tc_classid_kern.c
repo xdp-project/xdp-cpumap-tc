a9581cc9f018c1fe4f9406252560a767240c244a
Jesper Dangaard Brouer <netoptimizer@brouer.com>
2019-02-19 21:59:13 +0100

    Add tc_classid_kern.c
    Want to add more dynamic config, that can verify the classid.
    And eventually also set the classid based on a lookup table.
    
    Signed-off-by: Jesper Dangaard Brouer <netoptimizer@brouer.com>

---
+++ src/tc_classid_kern.c
@@ -0,0 +1,118 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/bpf.h>
+#include <linux/pkt_cls.h>
+#include <linux/pkt_sched.h> /* TC_H_MAJ + TC_H_MIN */
+
+#include "bpf_helpers.h"
+
+/* Manuel setup:
+
+ tc qdisc  del dev ixgbe2 clsact # clears all
+ tc qdisc  add dev ixgbe2 clsact
+ tc filter add dev ixgbe2 egress bpf da obj tc_classid_kern.o sec tc_class
+ tc filter list dev ixgbe2 egress
+
+*/
+#define MAX_CPUS 64
+
+/* iproute2 use another ELF map layout than libbpf.  The PIN_GLOBAL_NS
+ * will cause map to be exported to /sys/fs/bpf/tc/globals/
+ */
+#define PIN_GLOBAL_NS	2
+struct bpf_elf_map {
+        __u32 type;
+        __u32 size_key;
+        __u32 size_value;
+        __u32 max_elem;
+        __u32 flags;
+        __u32 id;
+        __u32 pinning;
+};
+
+/* More dynamic: let create a map that contains the mapping table, to
+ * allow more dynamic configuration.
+ *
+ * Lookup key: __u32 cpu;
+ */
+struct txq_config {
+	/* lookup key: __u32 cpu; */
+	__u16 queue_mapping;
+	__u16 htb_major;
+};
+
+struct bpf_elf_map SEC("maps") map_txq_config = {
+        .type	    = BPF_MAP_TYPE_ARRAY,
+        .size_key   = sizeof(__u32),
+        .size_value = sizeof(struct txq_config),
+        .pinning    = PIN_GLOBAL_NS,/* /sys/fs/bpf/tc/globals/map_txq_config */
+        .max_elem   = MAX_CPUS,
+};
+
+/*
+  CPU config map table (struct txq_config):
+
+  |----------+---------------+-----------+-----------------|
+  | Key: CPU | queue_mapping | htb_major | maps-to-MQ-leaf |
+  |----------+---------------+-----------+-----------------|
+  |        0 |             1 |      100: | 7FFF:1          |
+  |        1 |             2 |      101: | 7FFF:2          |
+  |        2 |             3 |      102: | 7FFF:3          |
+  |        3 |             4 |      103: | 7FFF:4          |
+  |----------+---------------+-----------+-----------------|
+
+  Last column "maps-to-MQ-leaf" is not part of config, but illustrates
+  that queue_mapping corresponds to MQ-leaf "minor" numbers, assuming
+  MQ is created with handle 7FFF, like:
+
+   # tc qdisc replace dev ixgbe2 root handle 7FFF: mq
+
+  The HTB-qdisc major number "handle" is choosen by the user, when
+  attaching the HTB qdisc to the MQ-leaf "parent", like:
+
+   # tc qdisc add dev ixgbe2 parent 7FFF:1 handle 100: htb default 2
+   # tc qdisc add dev ixgbe2 parent 7FFF:2 handle 101: htb default 2
+
+ */
+
+
+#define DEBUG 1
+#ifdef  DEBUG
+/* Only use this for debug output. Notice output from bpf_trace_printk()
+ * end-up in /sys/kernel/debug/tracing/trace_pipe
+ */
+#define bpf_debug(fmt, ...)                                             \
+                ({                                                      \
+                        char ____fmt[] = fmt;                           \
+                        bpf_trace_printk(____fmt, sizeof(____fmt),      \
+                                     ##__VA_ARGS__);                    \
+                })
+#else
+#define bpf_debug(fmt, ...) { } while (0)
+#endif
+
+/* Wrap the macros from <linux/pkt_sched.h> */
+#define TC_H_MAJOR(x) TC_H_MAJ(x)
+#define TC_H_MINOR(x) TC_H_MIN(x)
+
+
+SEC("tc_class")
+int  tc_cls_prog(struct __sk_buff *skb)
+{
+	__u32 cpu = bpf_get_smp_processor_id();
+	__u16 txq_root_handle;
+
+	/* The skb->queue_mapping is 1-indexed (zero means queue_mapping not
+	 * set).  The underlying MQ leaf's are also 1-indexed, which makes it
+	 * easier to reason about.
+	 */
+	txq_root_handle = cpu + 1;
+	skb->queue_mapping = txq_root_handle;
+
+	// TODO: Verify that the TC handle major number in
+	// skb->priority field is correct.
+
+	return TC_ACT_OK;
+}
+
+
+char _license[] SEC("license") = "GPL";
