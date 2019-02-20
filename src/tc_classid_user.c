76e0c1c09fc7b9a70b14c6deb07c1746cba4fa3e
Jesper Dangaard Brouer <netoptimizer@brouer.com>
2019-02-20 09:26:21 +0100

    Add tc_classid_user
    Signed-off-by: Jesper Dangaard Brouer <netoptimizer@brouer.com>

---
+++ src/tc_classid_user.c
@@ -0,0 +1,45 @@
+static const char *__doc__=
+ " TC: Control program for tc_classid_kern.o\n";
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <linux/types.h>
+
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+
+#include "common.h"
+
+static int map_txq_config_fd = -1;
+
+int open_bpf_map_file(const char *file)
+{
+	int fd;
+
+	fd = bpf_obj_get(file);
+	if (fd < 0) {
+		fprintf(stderr,
+			"WARN: Failed to open bpf map file:%s err(%d):%s\n",
+			file, errno, strerror(errno));
+		return fd;
+	}
+	return fd;
+}
+
+int main(int argc, char **argv)
+{
+	printf("Hello World, map name: %s\n", file_txq_config);
+
+	if ((map_txq_config_fd = open_bpf_map_file(file_txq_config)) < 0) {
+		fprintf(stderr,
+			"ERR: cannot proceed without access to config map\n");
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
+
