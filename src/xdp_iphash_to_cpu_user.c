xdp-project/xdp-cpumap-tc

commit 582edcb7026f708afb712cf28d89801f2c820cfe

cpumap qsize: allow changing and set new larger default
Experience from production shows queue size of 192 is too small, as
this caused packet drops during cpumap-enqueue on RX-CPU.  This can be
diagnosed with xdp_monitor sample program.

This bpftrace program was used to diagnose the problem in more detail:

 bpftrace -e '
  tracepoint:xdp:xdp_cpumap_kthread { @deq_bulk = lhist(args->processed,0,10,1); @drop_net = lhist(args->drops,0,10,1) }
  tracepoint:xdp:xdp_cpumap_enqueue { @enq_bulk = lhist(args->processed,0,10,1); @enq_drops = lhist(args->drops,0,10,1); }'

Watch out for the @enq_drops counter. The @drop_net counter can happen
when netstack gets invalid packets, so don't despair it can be
natural, and that counter will likely disappear in newer kernels as it
was a source of confusion (look at netstat info for reason of the
netstack @drop_net counters).

The production system was configured with CPU power-saving C6 state.
Learn more in this blogpost[1].

And wakeup latency in usec for the states are:

 # grep -H . /sys/devices/system/cpu/cpu0/cpuidle/*/latency
 /sys/devices/system/cpu/cpu0/cpuidle/state0/latency:0
 /sys/devices/system/cpu/cpu0/cpuidle/state1/latency:2
 /sys/devices/system/cpu/cpu0/cpuidle/state2/latency:10
 /sys/devices/system/cpu/cpu0/cpuidle/state3/latency:133

Deepest state take 133 usec to wakeup from (133/10^6). The link speed
is 25Gbit/s ((25*10^9/8) in bytes/sec). How many bytes can arrive with
in 133 usec at this speed: (25*10^9/8)*(133/10^6) = 415625 bytes. With
MTU size packets this is 275 packets, and with minimum Ethernet (incl
intergap overhead) 84 bytes it is 4948 packets. Clearly default queue
size is too small.

Setting default cpumap queue to 2048 as worst-case (small packet) at
10Gbit/s is 1979 packets with 133 usec wakeup time, +64 packet before
kthread wakeup call (due to xdp_do_flush) worst-case 2043 packets.

Thus, if a packet burst on RX-CPU will enqueue packets to a remote

src/xdp_iphash_to_cpu_user.c
@@ -54,6 +54,7 @@ static const struct option long_options[] = {
 	{"owner",	required_argument,	NULL, 'o' },
 	{"skb-mode",	no_argument,		NULL, 'S' },
 	{"all-cpus",	no_argument,		NULL, 'a' },
+	{"qsize",	required_argument,	NULL, 's' },
 	{0, 0, NULL,  0 }
 };
 
@@ -444,13 +445,23 @@ int main(int argc, char **argv)
 
 	prog_load_attr_maps.pinned_maps = my_pinned_maps;
 
-	/* Notice: choosing the queue size is very important with the
-	 * ixgbe driver, because it's driver page recycling trick is
-	 * dependend on pages being returned quickly.  The number of
-	 * out-standing packets in the system must be less-than 2x
-	 * RX-ring size.
+	/* Notice: Choosing the queue size is very important when CPU is
+	 * configured with power-saving states.
+	 *
+	 * If deepest state take 133 usec to wakeup from (133/10^6). When link
+	 * speed is 10Gbit/s ((10*10^9/8) in bytes/sec). How many bytes can
+	 * arrive with in 133 usec at this speed: (10*10^9/8)*(133/10^6) =
+	 * 166250 bytes. With MTU size packets this is 110 packets, and with
+	 * minimum Ethernet (incl intergap overhead) 84 bytes is 1979 packets.
+	 *
+	 * Setting default cpumap queue to 2048 as worst-case (small packet)
+	 * should be +64 packet due kthread wakeup delay (due to xdp_do_flush)
+	 * worst-case is 2043 packets.
+	 *
+	 * Sysadm can configured system to avoid deep-sleep via:
+	 *   tuned-adm profile network-latency
 	 */
-	qsize = 128+64;
+	qsize = 2048;
 
 	/* Depend on sharing pinned maps */
 	if (bpf_fs_check_and_fix()) {
@@ -470,7 +481,7 @@ int main(int argc, char **argv)
 	prog_load_attr_maps.file = filename;
 
 	/* Parse commands line args */
-	while ((opt = getopt_long(argc, argv, "hSrqdwlc:",
+	while ((opt = getopt_long(argc, argv, "hSrqdwlc:q:",
 				  long_options, &longindex)) != -1) {
 		switch (opt) {
 		case 'q':
@@ -531,6 +542,9 @@ int main(int argc, char **argv)
 			break;
 		case 'a':
 			add_all_cpus = true;
+			break;
+		case 's':
+			qsize = strtoul(optarg, NULL, 0);
 			break;
 		case 'c':
 			add_all_cpus = false;

commit edc3b68d877293185db1e89e83021e54549b6525

xdp_iphash_to_cpu_user: Validate qsize input
On invalid input to strtoul() the value zero can be returned.
This qsize value is dangerous as the kernel will delete the
cpumap entry when giving qsize zero.

Fixes: 582edcb7026f ("cpumap qsize: allow changing and set new larger default")
Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

src/xdp_iphash_to_cpu_user.c
@@ -545,6 +545,11 @@ int main(int argc, char **argv)
 			break;
 		case 's':
 			qsize = strtoul(optarg, NULL, 0);
+			if (qsize == 0 || errno || (__s32)qsize < 0) {
+				fprintf(stderr, "ERR(%d): Invalid --qsize %d\n",
+					errno, qsize);
+				goto error;
+			}
 			break;
 		case 'c':
 			add_all_cpus = false;